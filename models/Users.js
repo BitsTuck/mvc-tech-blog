d90e52eb63965452d93c7b057eed8f02080a591a
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-11-06 10:22:01 -0500

    updated models and controllers

---
+++ models/Users.js
@@ -0,0 +1,15 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Users extends Model {}
+
+Users.init({
+    id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+    
+})
+

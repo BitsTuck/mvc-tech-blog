e937c91bfeae7b12e64f320b0b87d816765c45c4
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-11-16 08:51:10 -0500

    added new comment and post functionality, plus new blog entry routes

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -58,6 +58,30 @@ router.get('/post/:id', async (req, res) => {
 }
 });
 
+router.post('/post/new', async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            username: req.body.username
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(newPost)
+        });
+        const blogPost = newPost.get ({ plain: true })
+
+        res.render('/', {
+            blogPost
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err)
+    }
+})
+
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.render('/');
